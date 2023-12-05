8a8373095fed6f78eb1bbc6547fdeb7c8084a58c
TheJavaCoder <thejavaprogrammer3301@gmail.com>
2023-12-05 11:37:35 -0500

    Clean up and reorganize code
    Changed method names, got rid of comments and separated main class from heap class

--- Program.cs
+++ Program.cs
@@ -3,7 +3,7 @@ using System;
 using System.Diagnostics;
 using System.Xml.Linq;
 
-public class Program
+namespace Heap
 {
 	public class Heap
 	{
@@ -14,7 +14,7 @@ public class Program
 			Heap heap = new Heap();
 
 			heap.list.AddRange(initList);
-			heap.MinHeapify4();
+			heap.MinHeapify();
 
 			return heap;
 		}
@@ -55,13 +55,6 @@ public class Program
 			}
 		}
 
-		public void RebuildHeap()
-		{
-			Verify();
-			MinHeapify4();
-			Verify();
-		}
-
 		public bool Verify(int index = 0)
 		{
 			if (index >= list.Count)
@@ -87,12 +80,12 @@ public class Program
 
 		private int GetLeft(int index)
 		{
-			return 2 * index + 1;
+			return (2 * (index + 1)) - 1;
 		}
 
 		private int GetRight(int index)
 		{
-			return 2 * index + 2;
+			return 2 * (index + 1);
 		}
 
 		internal int GetParentIndex(int index)
@@ -100,72 +93,10 @@ public class Program
 			return ((index + 1) / 2) - 1;
 		}
 
-		//public void MinHeapify(int index = 0)
-		//{
-		//	int left = GetLeft(index + 1) - 1;
-		//	int right = GetRight(index + 1) - 1;
-		//	int smallest = index;
-
-		//	if (left < list.Count && list[left] < list[index])
-		//	{
-		//		smallest = left;
-		//	}
-		//	if (right < list.Count && list[right] < list[smallest])
-		//	{
-		//		smallest = right;
-		//	}
-
-		//	if (smallest != index)
-		//	{
-		//		(list[smallest], list[index]) = (list[index], list[smallest]);
-		//	}
-
-		//	if (left < list.Count)
-		//	{
-		//		MinHeapify(left);
-		//	}
-
-		//	if (right < list.Count)
-		//	{
-		//		MinHeapify(right);
-		//	}
-
-		//}
-
-		//public void MinHeapify2(int index = 0)
-		//{
-		//	int left = 2 * (index + 1) - 1;
-		//	int right = 2 * (index + 1);
-		//	int smallest = index;
-
-		//	Console.WriteLine($"Left Index: {left}; Right Index: {right}; Main Index: {index}; Left: {(left < list.Count ? list[left] : -1)}; Right: {(right < list.Count ? list[right] : -1)}; Main: {list[index]}");
-
-		//	if (left < list.Count && list[left] < list[index])
-		//	{
-		//		Console.WriteLine($"{list[left]} is less than {list[index]}");
-		//		(list[index], list[left]) = (list[left], list[index]);
-		//	}
-		//	if (right < list.Count && list[right] < list[index])
-		//	{
-		//		Console.WriteLine($"{list[right]} is less than {list[index]}");
-		//		(list[index], list[right]) = (list[right], list[index]);
-		//	}
-
-		//	//if (smallest != index)
-		//	//{
-		//	//	(list[smallest], list[index]) = (list[index], list[smallest]);
-		//	//}
-
-		//	if (left < list.Count)
-		//	{
-		//		MinHeapify2(left);
-		//	}
-
-		//	if (right < list.Count)
-		//	{
-		//		MinHeapify2(right);
-		//	}
-		//}
+		private void Log(int index, int parent)
+		{
+			Console.WriteLine($"\tSwapping {list[index]} (i: {index}) with {list[parent]} (i: {parent})");
+		}
 
 		private void Fix(int index)
 		{
@@ -173,6 +104,7 @@ public class Program
 
 			if (list[index] < list[parent])
 			{
+				Log(index, parent);
 				(list[index], list[parent]) = (list[parent], list[index]);
 			}
 
@@ -182,23 +114,25 @@ public class Program
 			}
 		}
 
-		public void MinHeapify4()
+		public void MinHeapify()
 		{
 			for (int j = 0; j < 2; j++)
 			{
+				Console.WriteLine($"Iteration {j + 1}");
+
 				for (int i = list.Count - 1; i > 0; i--)
 				{
 					Fix(i);
 				}
 			}
-			
+
 		}
 
 		public void Insert(int item)
 		{
 			list.Add(item);
 
-			MinHeapify4();
+			MinHeapify();
 		}
 
 		public int ExtractMinimum()
@@ -207,7 +141,7 @@ public class Program
 
 			list = list.GetRange(1, list.Count - 1);
 
-			MinHeapify4();
+			MinHeapify();
 
 			return minimum;
 		}
@@ -224,25 +158,24 @@ public class Program
 			return "\t" + string.Join(", ", list);
 		}
 	}
-
-	public static void Main(string[] args)
+	public class Program
 	{
-		List<int> values = new List<int>()
+		public static void Main(string[] args)
+		{
+			List<int> values = new List<int>()
 		{
 			20, 98, 97, 7, 60, 73, 40, 12, 84, 13, 85, 67, 84, 68, 30, 74, 20, 42, 64, 5, 7, 79, 12, 21, 24, 9, 23, 76, 63, 64, 54, 76, 31, 13, 43, 63, 88, 94, 10, 66, 91, 16, 17, 96, 3, 31, 49, 12, 69, 64, 100, 7, 85, 89, 10, 89, 97, 62, 42, 30, 58, 52, 62, 57, 59, 79, 83, 56, 63, 91, 22, 68, 45, 8, 58, 73, 92, 48, 11, 8, 80, 33, 38, 41, 51, 45, 5, 13, 27, 39, 24, 82, 67, 2, 63, 15, 35, 84, 25, 74
-			//12,11,10,9,8,7,6,5,4,3,2,1
+			//12,11,10,9,8,7,6,5,4,3,2,1,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32
 		};
 
-		Heap myHeap = Heap.FromList(values);
+			Heap myHeap = Heap.FromList(values);
 
-		myHeap.PrettyPrint();
+			Console.WriteLine($"Heap successful: {myHeap.Verify()}");
+			myHeap.PrettyPrint();
 
-		myHeap.MinHeapify4();
-		Console.WriteLine($"Heap successful: {myHeap.Verify()}");
-
-		//Console.WriteLine($"Heap successful: {myHeap.Verify(0)}");
-		//myHeap.RebuildHeap();
-
-		myHeap.PrettyPrint();
+			Console.WriteLine($"Heap successful: {myHeap.Verify()}");
+			myHeap.PrettyPrint();
+		}
 	}
 }
+
